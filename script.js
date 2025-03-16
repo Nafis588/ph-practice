026a8c716a1d190f0462c1e09c57bad6ac203bdc
Niloy <nafis.sadique.niloy@g.bracu.ac.bd>
2025-03-16 12:30:17 +0600

    refactor: update HTML structure and remove unused CSS; implement user data loading functionality

--- script.js
+++ script.js
@@ -1,43 +1,12 @@
-let video; 
-let canvas; 
-let selectedFilter = ""; 
-
-function setup() {
-    canvas = createCanvas(640, 480); 
-    canvas.parent("canvas-container"); 
-
-    video = createCapture(VIDEO); 
-    video.size(640, 480); 
-    video.hide();
+const loadUser = () => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(data => showUser(data))
 }
 
-function draw() {
-    image(video, 0, 0, width, height);
-
-    if (selectedFilter === "grayscale") {
-        filter(GRAY); 
-    } else if (selectedFilter === "invert") {
-        filter(INVERT); 
-    } else if (selectedFilter === "sepia") {
-
-        tint(112, 66, 20); 
-    } else if (selectedFilter === "reset") {
-        noTint(); 
+const showUser = (users) => {
+    for (let i = 0; i < users.length; i++) {
+        const user = users[i];
+        console.log(user);
     }
-}
-
-document.getElementById("grayscale-btn").addEventListener("click", () => {
-    selectedFilter = "grayscale";
-});
-
-document.getElementById("invert-btn").addEventListener("click", () => {
-    selectedFilter = "invert"; 
-});
-
-document.getElementById("sepia-btn").addEventListener("click", () => {
-    selectedFilter = "sepia"; 
-});
-
-document.getElementById("reset-btn").addEventListener("click", () => {
-    selectedFilter = "reset";
-});
+};
